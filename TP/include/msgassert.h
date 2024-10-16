f556b52a38f2a152b6685bd72c8274a18e44c9df
filipetexa <filipeteixeirasouza@gmail.com>
2024-10-16 08:45:11 -0300

    [update] add  ELEMSWAP definition directly on the macro file

--- TP/include/msgassert.h
+++ TP/include/msgassert.h
@@ -9,4 +9,7 @@
 #define erroAssert(e, m) ((void)((e) ? 0 : __erroassert(#e, __FILE__, __LINE__, m)))
 #define __erroassert(e, file, line, m) ((void)fprintf(stderr, "%s:%u: Erro '%s' - %s\n", file, line, e, m), abort(), 0)
 
+// Macro que realiza swap sem variavel auxiliar
+#define ELEMSWAP(x, y) (x += y, y = x - y, x -= y)
+
 #endif // MSGASSERT_H
